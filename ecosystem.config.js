627d334eb844db0aadd99481ce7e88fd2d4d38e9
AaronM <info@aamservices.uk>
2019-02-27 17:49:54 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-	apps : [{
-            name: 'loopback',
-            script: 'npm',
-            args: 'start',
-            exec_model: 'fork',
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '4G'
-            }]
-       };
+   module.exports = {
+     apps: [
+       {
+         name: 'loopback',
+         script: 'index.js',
+         instances: 1,
+         autorestart: true,
+         watch: false,
+         max_memory_restart: '1G',
+         env: {
+           NODE_ENV: 'development',
+         },
+         env_production: {
+           NODE_ENV: 'production',
+         },
+       },
+     ],
+   };
